f11b0e4eb7286622475ec066553d4f6acb0f9a61
Gerardo Ibarra <gibarra@upgradeit.com.ar>
2020-06-11 13:32:38 -0300

    support typescript

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -14,8 +14,12 @@
     </div>
 </template>
 
-<script>
-    export default {
+<script lang="ts">
+    import Vue from "vue"
+    import Component from "vue-class-component"
+
+    @Component
+    export default class ExampleComponent extends Vue {
         mounted() {
             console.log('Component mounted.')
         }
